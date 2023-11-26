9301268215c551f2aa7e460f99703dac1534efef
Timo P <t.p.fed@proton.me>
2023-11-26 11:34:04 +0100

    Create App.java
    I don't know why I made this but this is the first thing I did on my own!

---
+++ App.java
@@ -0,0 +1,16 @@
+// Tmo :)
+
+class Test {
+    public static void main(String[] args) {
+
+        double Hours = x; //enter your weekly workhours
+        double Pay = x; //enter your hourly pay
+
+        int a = x; //enter monthly / bi-weekly pay in weeks (2 or 4)
+      
+        double Money = (Hours * Pay);
+        double ActualPay = (Money * a);
+        System.out.println("At my current job, I get paid = " + ActualPay + "€");
+
+    }
+}
